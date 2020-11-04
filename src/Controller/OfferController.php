3d727d357286c0220d485568fd7a56f78c2fdd29
Juliette <juliette.bois@infomaniak.com>
2020-11-04 15:56:20 +0100

    Fix bug on return page

--- src/Controller/OfferController.php
+++ src/Controller/OfferController.php
@@ -2,8 +2,6 @@
 
 namespace App\Controller;
 
-use App\Entity\Offer;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,20 +10,9 @@ class OfferController extends AbstractController
 {
     /**
      * @Route("/offer", name="offer")
-     * @param EntityManagerInterface $em
-     * @return Response
      */
-    public function index(EntityManagerInterface $em): Response
+    public function index(): Response
     {
-        $offer = new Offer();
-        $offer->setTitle('chatons');
-        $offer->setDescription('chatons à donner');
-        $offer->setCategory('animaux');
-        $offer->setPrice('0');
-
-        //$em->persist($offer);
-        //$em->flush();
-
         return $this->render('offer/index.html.twig', [
             'controller_name' => 'OfferController',
         ]);
