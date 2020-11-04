910a73b0fe6e903bc933ea1ef14e6e090ca207b3
Juliette <juliette.bois@infomaniak.com>
2020-11-04 15:52:20 +0100

    Make style with tailwind (WIP)

--- src/Controller/ReportedOffersController.php
+++ src/Controller/ReportedOffersController.php
@@ -6,15 +6,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController extends AbstractController
+class ReportedOffersController extends AbstractController
 {
     /**
-     * @Route("/", name="home")
+     * @Route("/reported/offers", name="reported_offers")
      */
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+        return $this->render('reported_offers/index.html.twig', [
+            'controller_name' => 'ReportedOffersController',
         ]);
     }
 }
--- src/Controller/SendMessageController.php
+++ src/Controller/SendMessageController.php
@@ -6,15 +6,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController extends AbstractController
+class SendMessageController extends AbstractController
 {
     /**
-     * @Route("/", name="home")
+     * @Route("/send/message", name="send_message")
      */
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+        return $this->render('send_message/index.html.twig', [
+            'controller_name' => 'SendMessageController',
         ]);
     }
 }
