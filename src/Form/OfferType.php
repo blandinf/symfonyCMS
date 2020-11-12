e0b9e59f2e3df42647aef82ca87ca2bdc810f65c
Florian Blandin <pro.blandinf@gmail.com>
2020-11-12 21:54:37 +0100

    CRUD offer (handle media, choice between db categories..))

---
+++ src/Form/OfferType.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Category;
+use App\Entity\Offer;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class OfferType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title', TextType::class)
+            ->add('description',TextType::class)
+            ->add('price', NumberType::class)
+            ->add('category', EntityType::class, [
+                'class' => Category::class,
+                'multiple' => true,
+                'choice_label' => 'name',
+            ])
+            ->add('image', FileType::class, [
+                'label' => false,
+                'multiple' => false,
+                'mapped' => false,
+                'required' => false
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Offer::class,
+        ]);
+    }
+}
